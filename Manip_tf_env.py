64a2e6dfc807d10e18bffbfa2326797974b5207f
GuestKP_mainPC <72121410+GuestKP@users.noreply.github.com>
2020-10-30 18:47:52 +0700

    rewards not working

--- Manip_tf_env.py
+++ Manip_tf_env.py
@@ -1,77 +1,71 @@
 import pygame
 import _thread
-from random import shuffle
 from time import sleep
+import numpy as np
 
 
 class Manipulator:
-    K = 10
-    COLORS = [None, 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'magenta']
+    K = 4
+    COLORS = list(
+        set([l.translate({ord(i): None for i in '0123456789'}) for l in list(pygame.colordict.THECOLORS.keys())]))
 
     def __init__(self, n_boxes):
-        tmp = 512/((n_boxes+1)*6+1)
+        tmp = 512/((n_boxes)*6+1)
         self.SIZES = {'gap': tmp, 'sum': 6*tmp, 'box': 5*tmp}
-        self.boxes = [i for i in range(n_boxes+1)]
+        self.boxes = np.array(list(range(n_boxes)))
+        self.need = self.boxes.copy()
         self.n_boxes = n_boxes
-        self.frame = 0
         self.window = None
         self.need_render = -1
-        self.thread2 = _thread.start_new_thread(self.tread_main, ())
+        # self.thread2 = _thread.start_new_thread(self.tread_main, ())
         # 1. почему нужет thread:
         #    если Manipulator.render вызвано ОДНАЖДЫ, то без thread окошко зависает.
         # 2. почему в Manipulator.render находится цикл с sleep(0):
         #    создание окошка зависает при любых командах (я хз почему),
         #    кроме sleep() и print()
-        self.reset()
+        # self.reset()
 
     def reset(self):
-        shuffle(self.boxes)
-        self.frame = 0
+        np.random.shuffle(self.boxes)
         return self.boxes
 
     def count_sum(self):
-        return sum([(0 if idx+1 == box else -self.K) for idx, box in enumerate(self.boxes[:-1])]) - self.frame
+        return -np.abs(self.boxes - self.need).sum()
 
-    def step(self, pos):
-        self.frame += 1
-        if self.boxes[pos[0]] == 0 or self.boxes[pos[1]] != 0:  # if nothing in "From" or smth in "To"
-            s = self.count_sum() - self.K*self.n_boxes  # penalty for error move
-        else:
-            self.boxes[pos[1]] = self.boxes[pos[0]]
-            self.boxes[pos[0]] = 0  # move box
-            s = self.count_sum()
-        return self.boxes, s
+    def step(self, fr, to):
+        self.boxes[to], self.boxes[fr] = self.boxes[fr], self.boxes[to]
+        s = self.count_sum()
+        return self.boxes, s, s == 0
 
     def tread_main(self):
         pygame.init()
         while True:
-            if self.need_render >= 0:
+            if self.need_render == 0:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        self.window = None
+                        self.need_render = False
+
+            if self.need_render == 1:
                 if self.window is None:
                     self.window = pygame.display.set_mode((512, 528))
                     pygame.display.set_caption("Labyrinth")
 
                 self.window.fill(pygame.Color("black"))
                 for pos, box in enumerate(self.boxes):
-                    if box != 0:
-                        pygame.draw.rect(self.window, pygame.Color(self.COLORS[box]),
-                                         (self.SIZES['gap']+pos*self.SIZES['sum'],
-                                          512-self.SIZES['sum'], self.SIZES['box'], self.SIZES['box']))
-                    if pos != self.n_boxes:
-                        pygame.draw.line(self.window, pygame.Color(self.COLORS[pos+1]),
-                                         (self.SIZES['gap']+pos*self.SIZES['sum'], 520),
-                                         ((pos+1)*self.SIZES['sum'], 520), 5)
+                    pygame.draw.rect(self.window, pygame.Color(self.COLORS[box]),
+                                     (self.SIZES['gap']+pos*self.SIZES['sum'],
+                                      512-self.SIZES['sum'], self.SIZES['box'], self.SIZES['box']))
+                    pygame.draw.line(self.window, pygame.Color(self.COLORS[pos]),
+                                     (self.SIZES['gap']+pos*self.SIZES['sum'], 520),
+                                     ((pos+1)*self.SIZES['sum'], 520), 5)
                 pygame.display.update()
                 pygame.display.flip()
 
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        self.window = None
-                        self.need_render = False
                 self.need_render = 0
 
     def render(self):
-        if self.need_render == -1:
-            self.need_render = 1
-            while self.need_render == 1:
-                sleep(0)
+        self.need_render = 1
+        while self.need_render == 1:
+            sleep(0)
--- Manip_tf_train.py
+++ Manip_tf_train.py
@@ -1,19 +1,140 @@
 from Manip_tf_env import Manipulator
-from time import time
+# from new import Storage
+from time import sleep, time
+from random import randint
 
-env = Manipulator(8)
-env.render()
-state = env.reset()
+import gym
+import tensorflow as tf
+import tf_slim as slim
+from tensorflow.python.saved_model import tag_constants
+import numpy as np
 
-a = True
-t = time()
-while True:
-    if time()-t > 2:
-        if state.index(0)+1 != len(state):
-            act = [state.index(state.index(0)+1), state.index(0)]
-        else:
-            act = [[idx+1 != box for idx, box in enumerate(state)].index(True), len(state)-1]
-        state_new, reward = env.step(act)
-        print(state, act, state_new, reward)
-        state = state_new
-        t = time()
+
+try:
+    xrange = xrange
+except:
+    xrange = range
+
+gamma = 0.5  # коэффициент дисконтирования
+
+def discount_rewards(r):
+    discounted_r = np.zeros_like(r)
+    running_add = 0
+    for t in reversed(xrange(0, r.size)):
+        running_add = running_add * gamma + r[t]
+        discounted_r[t] = running_add
+    return discounted_r
+
+
+class Agent():
+    def __init__(self, lr, s_size, a_size, h_size):
+        # Ниже инициализирована feed-forward часть нейросети.
+        # Агент оценивает состояние среды и совершает действие
+        self.state_in = tf.placeholder(shape=[None, s_size], dtype=tf.float32)
+        hidden = slim.fully_connected(self.state_in, h_size,
+                                      biases_initializer=None, activation_fn=tf.nn.relu)
+        hidden_2 = slim.fully_connected(hidden, h_size,
+                                       biases_initializer=None, activation_fn=tf.nn.relu)
+        self.output = slim.fully_connected(hidden_2, a_size,
+                                           activation_fn=tf.nn.softmax, biases_initializer=None)
+        self.chosen_action = tf.argmax(self.output, 1)  # выбор действия
+
+        # Следующие 6 строк устанавливают процедуру обучения.
+        # Нейросеть принимает на вход выбранное действие
+        # и соответствующий выигрыш,
+        # чтобы оценить функцию потерь и обновить веса модели.
+        self.reward_holder = tf.placeholder(shape=[None], dtype=tf.float32)
+        self.action_holder = tf.placeholder(shape=[None], dtype=tf.int32)
+
+        self.indexes = tf.range(0, tf.shape(self.output)[0]) * tf.shape(self.output)[1] + self.action_holder
+
+        self.responsible_outputs = tf.gather(tf.reshape(self.output, [-1]),
+                                             self.indexes)
+        # функция потерь
+        self.loss = -tf.reduce_mean(tf.log(self.responsible_outputs) *
+                                    self.reward_holder)
+
+        tvars = tf.trainable_variables()
+        self.exported = tf.trainable_variables()
+        self.gradient_holders = []
+        for idx, var in enumerate(tvars):
+            placeholder = tf.placeholder(tf.float32, name=str(idx) + '_holder')
+            self.gradient_holders.append(placeholder)
+
+        self.gradients = tf.gradients(self.loss, tvars)
+
+        optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+        self.update_batch = optimizer.apply_gradients(zip(self.gradient_holders,
+                                                          tvars))
+
+
+tf.reset_default_graph()  # Очищаем граф tensorflow
+
+boxes = 4
+actions = [[i, j] for i in range(boxes) for j in range(boxes) if i != j]
+myAgent = Agent(lr=0.5, s_size=boxes, a_size=boxes*(boxes-1), h_size=32)  # Инициализируем агента
+# myAgent = Agent(lr=1e-2, s_size=4, a_size=2, h_size=8)  # Инициализируем агента
+saver = tf.train.Saver()
+
+# env = Storage(32, 32, ex='storage.txt')
+env = Manipulator(boxes)
+# env = gym.make('CartPole-v0')
+choice = list(range(boxes))
+
+total_episodes = 5000  # Количество итераций обучения
+max_ep = 200
+update_frequency = 50
+winned_eps = 0
+init = tf.global_variables_initializer()
+
+print('Point 1')
+
+with tf.Session() as sess:
+    sess.run(init)
+    total_reward = []
+    gradBuffer = sess.run(tf.trainable_variables())
+    for ix, grad in enumerate(gradBuffer):
+        gradBuffer[ix] = grad * 0
+
+    for i in range(total_episodes):
+        st = env.reset()
+        sum_rew = 0
+        ep_history = []
+        for j in range(max_ep):
+            a_dist = sess.run(myAgent.output, feed_dict={myAgent.state_in: [st]})[0]
+            act = a_dist.argmax()
+            # act = np.random.choice(range(len(a_dist)), p=a_dist)
+            st1, rew, done = env.step(actions[act][0], actions[act][1])  # Получить награду за совершенное действие
+            # st1, rew, done, _ = env.step(act)
+            ep_history.append([st, act, rew, st1])
+            st = st1
+            sum_rew += rew
+            if done:
+                winned_eps += 1
+                ep_history = np.array(ep_history)
+                ep_history[:, 2] = discount_rewards(ep_history[:, 2])
+                feed_dict = {myAgent.reward_holder: ep_history[:, 2],
+                             myAgent.action_holder: ep_history[:, 1],
+                             myAgent.state_in: np.vstack(ep_history[:, 0])}
+                grads = sess.run(myAgent.gradients, feed_dict=feed_dict)
+                for idx, grad in enumerate(grads):
+                    gradBuffer[idx] += grad
+
+                if i % update_frequency == 0 and i != 0:
+                    feed_dict = dictionary = dict(zip(myAgent.gradient_holders,
+                                                      gradBuffer))
+                    _ = sess.run(myAgent.update_batch, feed_dict=feed_dict)
+                    for ix, grad in enumerate(gradBuffer):
+                        gradBuffer[ix] = grad * 0
+
+                total_reward.append(sum_rew)
+                break
+
+        if j == max_ep-1:
+            total_reward.append(sum_rew - 50)
+
+        if i % 100 == 0 and i != 0:
+            print(np.mean(total_reward[-100:]), '\t--- Progress: ', ((i) * 100 / total_episodes),
+                  '% --- Done param: ', len(total_reward), ' from ', total_episodes, 'episodes (', winned_eps, ' winned)')
+            winned_eps = 0
+    pass
